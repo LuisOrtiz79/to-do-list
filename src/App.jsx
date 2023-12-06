6a5240abce1448dcd4a081e6b01d35ca47c9e00f
Luis Ortiz <luis.ortiz7905@gmail.com>
2023-12-06 18:42:54 -0400

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css'
 import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
-    <>
       <div className='App'>
-        <HomePage />
+        <Navbar />
+
+        <div className='app-container'>
+          
+          <Sidebar />
+
+          <div className='routes'>
+              
+              <HomePage />
+
+          </div>
+
+        </div>
+
+        <Footer />
       </div>
-    </>
   )
 }
 
