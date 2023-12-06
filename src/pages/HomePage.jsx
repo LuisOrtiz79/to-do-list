f78833c729e4ab48d8603ba7d74345bfc163c6e9
Luis Ortiz <luis.ortiz7905@gmail.com>
2023-12-06 18:43:24 -0400

    Second commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,3 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
 import TaskCard from "../components/taskCard";
 import taskData from "../task.json";
 import { useState } from "react";
@@ -9,18 +6,12 @@ function HomePage(){
     const [tasks, setTasks] = useState(taskData);
     return (
         <>
-            <Navbar />
-
             <div>To-Do List</div>
 
             {tasks &&
                 tasks.map((task) => {
                 return <TaskCard task={task}/>;
             })}
-
-            <Sidebar />
-
-            <Footer />
         </>
 
     )
