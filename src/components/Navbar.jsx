efe479433a78c4e5c4c93b9b3effe5ea4b327a65
Luis Ortiz <luis.ortiz7905@gmail.com>
2023-12-07 18:28:18 -0400

    Styles and Navbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,30 @@
 import Button from './Button';
+import logo from '../assets/img-logo_480.png'
 
 const Navbar = () => {
   return (
-    <div className="Navbar">
-    <nav>
-      <p>To do List</p>
-      
-      <Button />
-      <Button />        
+    <nav className="Navbar">
+      <div className="Nav2">
+        <div className="Nav3">
+
+          <button className="button-img">
+            <img src={logo} alt="Logo" className="img-logo" />
+          </button>
+        </div>
+
+        <div className="Nav4">
+        <span className="text-xl">
+            To do App
+          </span>
+        </div>
+      </div>
+                {/* <nav>
+                  <p>To do List</p>
+                  
+                  <Button />
+                  <Button />        
+                </nav> */}
     </nav>
-    </div>
   )
 }
 
