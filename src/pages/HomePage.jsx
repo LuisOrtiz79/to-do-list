70c10b3f3fbbffa5d47c97175864963ea83e1684
Luis Ortiz <luis.ortiz7905@gmail.com>
2023-12-07 16:35:21 -0400

    Added form, delete and some styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
-import TaskCard from "../components/taskCard";
+import TaskCard from "../components/TaskCard";
+import AddTask from "../components/AddTask";
 import taskData from "../task.json";
 import { useState } from "react";
+import DeleteTask from "../components/DeleteTask";
 
 function HomePage(){
     const [tasks, setTasks] = useState(taskData);
     return (
         <>
             <div>To-Do List</div>
+
+            <AddTask tasks={tasks} setTasks={setTasks} />
 
             {tasks &&
                 tasks.map((task) => {
