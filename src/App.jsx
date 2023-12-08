98118304df402f5106c7fa92a8dbc004e6ca67e8
Luis Ortiz <luis.ortiz7905@gmail.com>
2023-12-07 22:13:07 -0400

    Added router and link to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import TaskDetailPage from './pages/TaskDetailPage';
+import NotFoundPage from './pages/NotFoundPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
@@ -15,8 +19,13 @@ function App() {
           <Sidebar />
 
           <div className='routes'>
-              
-              <HomePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} /> 
+            <Route path="/tasks" element={<TaskDetailPage />} />
+            <Route path="/about" element={<AboutPage />} />
+
+            <Route path="*" element={ <NotFoundPage /> } />
+          </Routes>
 
           </div>
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,39 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Sidebar = () => {
   return (
     <>
       <div>Sidebar</div>
 
-      <div>
+      <ul className="sidebar">
+        {/* <Link to="/"> Home </Link>
+        <Link to="/tasks"> Tasks </Link>
+        <Link to="/about"> About </Link> */}
+
+        <NavLink to="/" className={({ isActive }) => isActive ? "selected" : ""}>
+          Home
+        </NavLink>
+        
+        <NavLink 
+          to="/tasks" 
+          className={({ isActive }) => isActive ? "selected" : ""}
+         >
+          Tasks
+        </NavLink>
+        
+        <NavLink 
+          to="/about" 
+          className={({ isActive }) => isActive ? "selected" : ""}
+        >
+          About
+        </NavLink>
+      </ul>
+
+      {/* <div>
         <h5>HomePage</h5>
-        <br/>
         <h5>Add Task</h5>
-        <br/>
         <h5>Show Completed</h5>
-      </div>
+      </div> */}
     </>
   )
 }
